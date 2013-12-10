4064f95f5fa540d09a65ddea6b105efad58c2146
Henry Darmajaya <hdarmajaya@atni.com>
2013-12-10 09:49:00 -0500

    Add views for scdr_counters

--- app/controllers/scdr_counter_controller.rb
+++ app/controllers/scdr_counter_controller.rb
@@ -1,4 +1,12 @@
 class ScdrCounterController < ApplicationController
+
   def index
+    @date = DateTime.now.beginning_of_week
+    @current_week = @date.strftime("%U").to_i
+    filename = ''
+    @daily_counters = ScdrCounter.by_day(@date, @date.end_of_week, filename)
+    @hourly_counters = ScdrCounter.by_hour(@date, @date.end_of_week, filename)
+    @file_counters = ScdrCounter.by_file(@date, @date.end_of_week, filename)
   end
+
 end
