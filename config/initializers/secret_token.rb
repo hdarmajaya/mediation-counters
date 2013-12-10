b426f8694d070e50dcb11fbcd3190e6ddffe45dd
Henry Darmajaya <hdarmajaya@atni.com>
2013-12-09 20:24:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MediationCounters::Application.config.secret_token = '128ffc1385126868eb38e513b8baea34d9901c1f8a7fd81a758df3fc9a364dab4fc364a53eacb56a268c06570caaf8110161f033a740f4c1e4ac14cb6a6afb87'
