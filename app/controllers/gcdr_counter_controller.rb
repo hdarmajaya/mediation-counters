939fb8ee66da679868949ce1797f0e5fc0694acc
Henry Darmajaya <hdarmajaya@atni.com>
2013-12-10 10:00:06 -0500

    Implement previous and next week functions

--- app/controllers/gcdr_counter_controller.rb
+++ app/controllers/gcdr_counter_controller.rb
@@ -1,7 +1,12 @@
 class GcdrCounterController < ApplicationController
 
   def index
-    @date = DateTime.now.beginning_of_week
+    if params[:start_date].blank?
+      @date = DateTime.now.beginning_of_week
+    else
+      @date = Date.parse(params[:start_date]).beginning_of_week
+    end
+
     @current_week = @date.strftime("%U").to_i
     filename = ''
     @daily_counters = GcdrCounter.by_day(@date, @date.end_of_week, filename)
